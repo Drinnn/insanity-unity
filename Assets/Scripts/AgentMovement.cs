99efc7cc5fc095c895c2a36813d3f308866d5270
Drinnn <pedro_lontro@hotmail.com>
2022-03-30 10:02:33 -0300

    feat: add basic player movement

--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class AgentMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float currentVelocity = 3;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     public void MoveAgent(Vector2 movementInput)
     {
-        Debug.Log(movementInput);
+        _rb.velocity = movementInput.normalized * 3;
     }
 }
