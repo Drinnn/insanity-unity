8a5a05fd28c7399b7ea5b161a573ece3a30bebfe
Drinnn <pedro_lontro@hotmail.com>
2022-03-30 10:20:08 -0300

    refactor: make player movement more fluid

--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -5,7 +5,12 @@ public class AgentMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
 
+    [field: SerializeField]
+    public MovementDataSO MovementData { get; set; }
+
     [SerializeField] private float currentVelocity = 3;
+
+    private Vector2 movementDirection;
 
     private void Awake()
     {
@@ -14,6 +19,29 @@ public class AgentMovement : MonoBehaviour
 
     public void MoveAgent(Vector2 movementInput)
     {
-        _rb.velocity = movementInput.normalized * currentVelocity;
+        if (movementInput.magnitude > 0)
+        {
+            movementDirection = movementInput.normalized;
+        }
+        currentVelocity = CalculateSpeed(movementInput);
+    }
+
+    private float CalculateSpeed(Vector2 movementInput)
+    {
+        if (movementInput.magnitude > 0)
+        {
+            currentVelocity += MovementData.acceleration * Time.deltaTime;
+        }
+        else
+        {
+            currentVelocity -= MovementData.deacceleration * Time.deltaTime;
+        }
+
+        return Mathf.Clamp(currentVelocity, 0, MovementData.maxSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = currentVelocity * movementDirection.normalized;
     }
 }
